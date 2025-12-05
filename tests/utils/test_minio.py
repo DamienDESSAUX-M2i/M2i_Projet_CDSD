2bdfb8f29628d804a1dfe1b22bfdc4f62d495912
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2025-12-05 17:01:10 +0100

    Add tests for minio.

---
+++ tests/utils/test_minio.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_get_client():
+    pass
+
+
+@pytest.mark.skip(reason="Not implemented yet")
+def test_make_buckets():
+    pass
